6b91c98da391c91631e24516beff9769a806f8f0
LimpBunion22 <r.daneelolivaw2@gmail.com>
2025-04-16 23:47:13 +0200

    initial

---
+++ design/src/main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to CONSULTUM" << "\n\n";
+    std::cout << "      --\n"
+              << "    /  o\\ \\" << "\n"
+              << "   |  /|   |" << "\n"
+              << "   \\   /\\   /" << "\n"
+              << "      --  " << "\n";
+    return 0;
+}
