d07123e1170e6bb51cb7fb1ba79620b8b443226d
LimpBunion22 <r.daneelolivaw2@gmail.com>
2025-04-21 15:01:36 +0200

    basic startpoint

--- design/src/main.cpp
+++ design/src/main.cpp
@@ -1,12 +1,33 @@
 
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Welcome to CONSULTUM" << "\n\n";
-    std::cout << "      --\n"
-              << "    /  o\\ \\" << "\n"
-              << "   |  /|   |" << "\n"
-              << "   \\   /\\   /" << "\n"
-              << "      --  " << "\n";
-    return 0;
+    std::string state = "Main Menu";
+
+    std::cout << "\n\n      Welcome to CONSULTUM" << "\n\n";
+    std::cout << "            ---\n"
+              << "          /  o\\ \\" << "\n"
+              << "         |  /|   |" << "\n"
+              << "          \\ / \\ /" << "\n"
+              << "            ---  " << "\n\n";
+
+
+    while (true){
+        std::cout << "\n" << state << ": ";
+        std::string input_cmd;
+        std::getline(std::cin, input_cmd);
+        
+        if(input_cmd.find("help") != std::string::npos){
+            std::cout << "\n      Help yourself, useless piece of sh*t" << "\n";
+            continue;
+        }
+
+        if(input_cmd.find("exit") != std::string::npos){
+            std::cout << "\n      See you soon!" << "\n";
+            return 0;
+        }
+
+
+    }
 }
